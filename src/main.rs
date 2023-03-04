afd380207b81220f73b66c02c26a998e35d0e9fe
daniel.schroeder <daniel.dn.schroeder@deutschebahn.com>
2023-03-04 09:39:00 +0100

    adds list of supported resource types as static array

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 use aws_sdk_cloudformation as cloudformation;
 use dialoguer::{console::Term, theme::ColorfulTheme, Select};
 use std::error::Error;
+mod supported_resource_types;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
+    println!("{:?}", supported_resource_types::SUPPORTED_RESOURCE_TYPES);
     let config = aws_config::load_from_env().await;
     let client = cloudformation::Client::new(&config);
     let stacks = get_stacks(&client).await?;
@@ -13,6 +15,10 @@ async fn main() -> Result<(), Box<dyn Error>> {
         .collect();
 
     let source_stack = select_stack("Select source stack", &stack_names)?;
+
+    let resources = get_resources(&client, source_stack).await?;
+
+    println!("{:?}", resources);
 
     let target_stack = select_stack("Select target stack", &stack_names)?;
 
@@ -48,4 +54,20 @@ fn select_stack<'a>(prompt: &str, items: &'a Vec<&str>) -> Result<&'a str, Box<d
         Some(index) => Ok(items[index]),
         None => Err("User did not select anything".into()),
     }
+}
+
+// function, which takes a stack name as string and returns all the resource of that stack
+async fn get_resources(
+    client: &cloudformation::Client,
+    stack_name: &str,
+) -> Result<Vec<cloudformation::model::StackResourceSummary>, cloudformation::Error> {
+    let resp = client
+        .list_stack_resources()
+        .stack_name(stack_name)
+        .send()
+        .await?;
+
+    let resources = resp.stack_resource_summaries().unwrap_or_default().to_vec();
+
+    Ok(resources)
 }
